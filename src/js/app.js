e8ef765965473b32e2a4f6b2fda4c5e156cd8870
Julita <bilajulia998@gmail.com>
2020-11-07 11:17:40 +0200

    work but dont show messages

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,26 @@
+import '../style.css';
 import $ from 'jquery';
-import Chat from './Chat';
+import Chat from './chat';
 
-// const chat = new Chat({
-//     onMessage: addRegister
-// })
+const chat = new Chat({
+    onMessage: showNewMessages,
+})
+
+const $register = $('#register');
+const $senderInput = $('#sender');
+const $messageInput = $('#message');
+
+$('#chatForm').on('submit', (e) => {
+    e.preventDefault();
+    sendMessage();
+});
+
+function sendMessage() {
+    chat.send($senderInput.val(), $messageInput.val());
+    $messageInput.val('');
+}
+
+function showNewMessages({ payload }) {
+    const $message = $(`<div>${payload.username}: ${payload.message}</div>`);
+    $register.append($message);
+}
--- src/js/chat.js
+++ src/js/chat.js
@@ -1,63 +1,17 @@
-// const socket = new WebSocket('wss://fep-app.herokuapp.com/');
+const SERVER_URL = ('wss://fep-app.herokuapp.com/');
 
-// console.log(socket);
-
-// socket.onopen = () => {
-//     console.log('open')
-//     socket.send('Hello world');
-// }
-
-// socket.onmessage = (message) => {
-//     console.log('message', JSON.parse(message.data));
-// }
-
-// document.getElementById('sendMsg').addEventListener('click', () => {
-//     socket.send(JSON.stringify({
-//         // JSON.stringify('clicked')
-//         type: 'message',
-//         payload: {
-//             username: 'Yuliia',
-//             message: 'SayHi'
-//         }
-//     }));
-// })
-
-// document.getElementById('sendMsg').addEventListener('click', () => {
-//     socket.send(JSON.stringify({
-//         // JSON.stringify('clicked')
-//         type: 'login',
-//         payload: 'Yuliia'
-//     }));
-// })
-
-const socket = new WebSocket('wss://fep-app.herokuapp.com/');
-
-console.log(socket)
-
-
-socket.onopen = () => {
-    console.log('socket opened');
-    // socket.send('Hello world!')
-}
-
-socket.onmessage = (message) => {
-    console.log('message', JSON.parse(message.data))
-}
-
-socket.onclose = () => {
-    console.log('close')
-}
-
-socket.onerror = () => {
-    console.log('error')
-}
-
-document.getElementById('sendMsg').addEventListener('click', () => {
-    socket.send(JSON.stringify({
-        type: 'message',
-        payload: {
-        username: "Yuliia",
-        message: "aga))"
+export default class Chat {
+    constructor() {
+        this.socket = new WebSocket(SERVER_URL);
     }
-    }));
-})
+
+    send(name, message){
+        this.socket.send(JSON.stringify({
+            type: 'message',
+            payload: {
+                username: name,
+                message: message,
+            }
+        }));
+    }
+}
